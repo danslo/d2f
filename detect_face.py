f6ebebee7f9eefa57c782d1418537d6073bac1db
Daniel Sloof <goapsychadelic@gmail.com>
2022-06-04 23:16:32 +0200

    experimentation

--- detect_face.py
+++ detect_face.py
@@ -9,24 +9,40 @@ def show_image(img):
     cv2.destroyAllWindows()
 
 def get_faces(img):
-    #lbp_face_cascade = cv2.CascadeClassifier('../Face-Detection-OpenCV/data/lbpcascade_frontalface.xml')
-    haar_face_cascade = cv2.CascadeClassifier('../Face-Detection-OpenCV/data/haarcascade_frontalface_alt.xml')
+    cascades = {
+       "haar_frontal": cv2.CascadeClassifier('../opencv/data/haarcascades/haarcascade_frontalface_default.xml'),
+#       "haar_profile": cv2.CascadeClassifier('../opencv/data/haarcascades/haarcascade_profileface.xml'),
+#       "lbp_profile": cv2.CascadeClassifier('../opencv/data/lbpcascades/lbpcascade_profileface.xml'),
+#       "lbp_frontal": cv2.CascadeClassifier('../opencv/data/lbpcascades/lbpcascade_frontalface_improved.xml'),
+    }
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #show_image(gray_img)
-    return haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
+
+    faces = []
+    for label, cascade in cascades.items():
+        if len(faces): break
+        for face in cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40), flags = cv2.CASCADE_SCALE_IMAGE):
+            faces.append(face)
+    return faces
 
 def detect_face(filename):
     img = cv2.imread(filename)
 
     num_faces = 0
-    for dimensions in [[280, 800, 0, 570], [280, 800, 1350, 570]]:
+
+    roi_areas = [
+        [700, 380, 0, 570],    # bottom left
+        [700, 380, 1350, 570], # bottom right
+        [0, 700, 0, 330],      # left stroke
+        [0, 700, 1590, 330],   # right stroke
+    ]
+    for dimensions in roi_areas:
         roi = img[
             dimensions[0]:dimensions[0]+dimensions[1],
             dimensions[2]:dimensions[2]+dimensions[3]
         ].copy()
         faces = get_faces(roi)
+        num_faces += len(faces)
         for (x, y, w, h) in faces:
-            num_faces += 1
             cv2.rectangle(
                 img,
                 (x+dimensions[2], y+dimensions[0]),
